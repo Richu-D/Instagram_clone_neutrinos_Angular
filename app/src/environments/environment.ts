b51e4430b4c87e2e2d3a1a9b549730e9fe134f75
Richu D <richu.developer@gmail.com>
2023-04-10 10:05:23 +0530

    home page completed with responsiveness

---
+++ app/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    "name": "dev",
+    "properties": {
+        "production": false,
+        "ssdURL": "http://localhost:8081/api/",
+        "tenantName": "brototype",
+        "appName": "instagramclone",
+        "namespace": "com.brototype.instagramclone",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web",
+        "NGFORAGE_MOBILE_DRIVER": "INDEXED_DB"
+    }
+}
